a9c7a7f09074ac2c77d7fcb0cca71104818f1828
Jackson Chakma <jacksonchakma260@gmail.com>
2026-03-15 01:06:57 +0600

    Log Out Google and Log In Google FUll compilete

---
+++ src/SingInWithFacebook/SingInWithFacebook.jsx
@@ -0,0 +1,11 @@
+ import React from 'react';
+ 
+ const SingInWithFacebook = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+ };
+ 
+ export default SingInWithFacebook;
--- src/SinginWithGoogle/SinginWithGoogle.jsx
+++ src/SinginWithGoogle/SinginWithGoogle.jsx
@@ -1,26 +1,51 @@
-import React from "react";
-// import app from "../Firebase/firebase.config";
-import { app } from "../Firebase/firebase.config";   // ✅ FIX
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import React, { useState } from "react";
+import { app } from "../Firebase/firebase.config";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 const SinginWithGoogle = () => {
+  const [user, setUser] = useState(null); //ui te dekanor jonno ata useState 
+
   const provider = new GoogleAuthProvider();
   const auth = getAuth(app);
 
-
+  // ✅  LOGIN (google)
   const SinginWithGooglebtnClick = () => {
-  signInWithPopup(auth, provider)
-    .then(result => {
-      console.log(result.user);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
+
+  // ✅ LOGOUT (google)
+  const singOutbtn = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
-      <button onClick={SinginWithGooglebtnClick}>Sing in With Google</button>
+      <img src={user?.photoURL} alt="photo" />
+      <h3>{user?.displayName}</h3>
+      <h3>{user?.email}</h3>
+
+      {user ? 
+        <button onClick={singOutbtn}>Log out</button> //judi user tak=ke tahole dekabe (log out)
+       :
+        <button onClick={SinginWithGooglebtnClick}>Sign in With Google</button>  // judi user na  take tahole dekabe  (Sign in With Google}
+      }
     </div>
   );
 };
